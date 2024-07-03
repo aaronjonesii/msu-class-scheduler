2ea8ac4cdc2142811e82ab39a89c594ed9828cb6
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-03 13:28:47 -0400

    feat: redirect logged in guard

---
+++ src/app/shared/guards/redirect-logged-in.guard.ts
@@ -0,0 +1,34 @@
+import { CanActivateFn, Router } from '@angular/router';
+import { inject } from "@angular/core";
+import { AuthService } from "../services/auth.service";
+import { catchError, map, of, take } from "rxjs";
+import { appRoutes } from "../../app.routes";
+import { LoggerService } from "../services/logger.service";
+
+export const RedirectLoggedInGuard: CanActivateFn = () => {
+  const auth = inject(AuthService);
+
+  const router = inject(Router);
+
+  const logger = inject(LoggerService);
+
+  return auth.authState$.pipe(
+    take(1),
+    map((user) => {
+      const isAuthenticated = !!user;
+
+      if (isAuthenticated) {
+        router.navigate([appRoutes.home]);
+      }
+
+      return !isAuthenticated;
+    }),
+    catchError((error: unknown) => {
+      logger.error('Error in RedirectSignedInGuard:', error);
+
+      router.navigate([appRoutes.error]);
+
+      return of(false);
+    }),
+  );
+};
