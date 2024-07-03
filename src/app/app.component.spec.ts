310fe4d19749ca5b89613fd42bfe6da2e4c3a9bf
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-03 10:47:29 -0400

    refactor: app layout structure

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,68 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import {
+  TopNavBarComponent
+} from "./shared/components/top-nav-bar/top-nav-bar.component";
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [
+        AppComponent,
+        TopNavBarComponent,
+      ],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
+  });
+
+  it('should render header', () => {
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.querySelector('header')).toBeTruthy();
+  });
+
+  it('should render csb-top-nav-bar', () => {
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    const header = compiled.querySelector('header')?.children[0];
+
+    expect(header?.tagName.toLowerCase()).toBe('csb-top-nav-bar');
+  });
+
+  it('should render main', () => {
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.querySelector('main')).toBeTruthy();
+  });
+
+  it('should render router-outlet', () => {
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+    const routerOutlet = compiled.querySelector('main')?.children[0];
+
+    expect(routerOutlet?.tagName.toLowerCase()).toBe('router-outlet');
+  });
+
+  it('should render footer', () => {
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.querySelector('footer')).toBeTruthy();
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,14 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {
+  TopNavBarComponent
+} from "./shared/components/top-nav-bar/top-nav-bar.component";
 
 @Component({
   selector: 'csb-root',
   standalone: true,
-  imports: [RouterOutlet],
-  template: `<router-outlet />`,
+  imports: [RouterOutlet, TopNavBarComponent],
+  templateUrl: './app.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AppComponent {}
