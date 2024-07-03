ab79d5d6e34d147287001910979903842eda407e
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-03 14:27:19 -0400

    feat: skeleton component

---
+++ src/app/shared/components/skeleton/skeleton.component.ts
@@ -0,0 +1,41 @@
+import {
+  ChangeDetectionStrategy,
+  Component, computed, input,
+} from '@angular/core';
+import { NgClass, NgStyle } from '@angular/common';
+
+@Component({
+  selector: 'csb-skeleton',
+  templateUrl: './skeleton.component.html',
+  styleUrl: './skeleton.component.scss',
+  standalone: true,
+  imports: [NgClass, NgStyle],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SkeletonComponent {
+  /** More details in https://angular.io/api/common/NgStyle */
+  styles = input<{ [k: string]: unknown; } | null>(null);
+
+  ariaLabel = input('loading');
+
+  appearance = input<'circle' | 'line' | 'custom-content' | ''>('line');
+
+  animation =
+    input<'progress' | 'progress-dark' | 'pulse' | 'false' | false>('progress');
+
+  loadingText = input('Loading...');
+
+  /** More details in https://angular.io/api/common/NgClass */
+  classes = input<{ [k: string]: unknown; } | null>(null);
+
+  computedClasses = computed(() => {
+    return {
+      'custom-content': this.appearance() === 'custom-content',
+      'circle': this.appearance() === 'circle',
+      'progress': this.animation() === 'progress',
+      'progress-dark': this.animation() === 'progress-dark',
+      'pulse': this.animation() === 'pulse',
+      ...(this.classes() ?? {})
+    };
+  });
+}
