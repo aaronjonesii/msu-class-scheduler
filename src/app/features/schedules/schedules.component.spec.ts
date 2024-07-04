799c30cdca5d8efcfa08ee9efa79f9e70e4ca386
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-04 17:35:37 -0400

    chore(schedules): get user schedules

--- src/app/features/schedules/schedules.component.spec.ts
+++ src/app/features/schedules/schedules.component.spec.ts
@@ -1,16 +1,37 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SchedulesComponent } from './schedules.component';
+import { SchedulesService } from "../../shared/services/schedules.service";
+import { AuthService } from "../../shared/services/auth.service";
+import SpyObj = jasmine.SpyObj;
+import createSpyObj = jasmine.createSpyObj;
+import { of } from "rxjs";
 
 describe('SchedulesComponent', () => {
   let component: SchedulesComponent;
   let fixture: ComponentFixture<SchedulesComponent>;
+  let mockAuthService: SpyObj<AuthService>;
+  let mockSchedulesService: SpyObj<SchedulesService>;
 
   beforeEach(async () => {
+    mockAuthService = createSpyObj('AuthService', ['authState$']);
+    mockSchedulesService = createSpyObj('SchedulesService', ['getByUser$'])
+
+    mockAuthService.authState$.and.returnValue(of(null));
+
     await TestBed.configureTestingModule({
-      imports: [SchedulesComponent]
-    })
-    .compileComponents();
+      imports: [SchedulesComponent],
+      providers: [
+        {
+          provide: SchedulesService,
+          useValue: mockSchedulesService,
+        },
+        {
+          provide: AuthService,
+          useValue: mockAuthService,
+        },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(SchedulesComponent);
     component = fixture.componentInstance;
@@ -19,5 +40,10 @@ describe('SchedulesComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show empty schedules when user is not authenticated', () => {
+    expect(mockSchedulesService.getByUser$).not.toHaveBeenCalled();
+    expect(component.schedules()).toEqual([]);
   });
 });
--- src/app/features/schedules/schedules.component.ts
+++ src/app/features/schedules/schedules.component.ts
@@ -1,11 +1,35 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+} from '@angular/core';
+import { SchedulesService } from "../../shared/services/schedules.service";
+import { toSignal } from "@angular/core/rxjs-interop";
+import { MatNavList } from "@angular/material/list";
+import { MatMenuItem } from "@angular/material/menu";
+import { AuthService } from "../../shared/services/auth.service";
+import { of, switchMap } from "rxjs";
 
 @Component({
   selector: 'csb-schedules',
   standalone: true,
-  imports: [],
+  imports: [
+    MatNavList,
+    MatMenuItem
+  ],
   templateUrl: './schedules.component.html',
   styleUrl: './schedules.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class SchedulesComponent {}
+export class SchedulesComponent {
+  private schedulesService = inject(SchedulesService);
+  private authService = inject(AuthService);
+
+  schedules = toSignal(
+    this.authService.authState$().pipe(
+      switchMap((user) => {
+        return user ? this.schedulesService.getByUser$(user.uid) : of([]);
+      }),
+    ),
+  );
+}
