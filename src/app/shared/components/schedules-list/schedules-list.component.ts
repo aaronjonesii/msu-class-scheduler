ee1de0de4a78fdb1a7e948f73eb9b76e6812f4c3
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-06 17:44:02 -0400

    refactor(schedules-list): update to masonry cards

--- src/app/shared/components/schedules-list/schedules-list.component.ts
+++ src/app/shared/components/schedules-list/schedules-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, inject, input } from '@angular/core';
 import {
   ReadScheduleWithClasses
 } from "../../interfaces/schedule";
@@ -12,7 +12,12 @@ import {
 import { MatMenuItem } from "@angular/material/menu";
 import { RouterLink } from "@angular/router";
 import { SkeletonComponent } from "../skeleton/skeleton.component";
-import { ReadScheduleClass } from "../../interfaces/schedule-class";
+import { MatCardModule } from "@angular/material/card";
+import { MatChip, MatChipSet } from "@angular/material/chips";
+import { ColorToClassPipe } from "../../pipes/color-to-class.pipe";
+import { MatAnchor, MatButton } from "@angular/material/button";
+import { SchedulesService } from "../../services/schedules.service";
+import { MatIcon } from "@angular/material/icon";
 
 @Component({
   selector: 'csb-schedules-list',
@@ -24,23 +29,28 @@ import { ReadScheduleClass } from "../../interfaces/schedule-class";
     MatMenuItem,
     MatNavList,
     RouterLink,
-    SkeletonComponent
+    SkeletonComponent,
+    MatCardModule,
+    MatChipSet,
+    MatChip,
+    ColorToClassPipe,
+    MatButton,
+    MatAnchor,
+    MatIcon,
   ],
   templateUrl: './schedules-list.component.html',
   styleUrl: './schedules-list.component.scss'
 })
 export class SchedulesListComponent {
+  private scheduleService = inject(SchedulesService);
+
   protected readonly appRoutes = appRoutes;
 
   schedules = input<ReadScheduleWithClasses[] | null | undefined>(null);
 
-  scheduleClassesText = (scheduleClasses: ReadScheduleClass[]) => {
-    if (!scheduleClasses.length) return 'Classes: No classes yet';
+  filterText  = input<string>();
 
-    const scheduleClassesText = scheduleClasses.map((c) => {
-      return c?.subjectCode && c?.courseNumber ? `` : c.name;
-    });
-
-    return `Classes: ${scheduleClassesText.join(', ')}`;
-  };
+  editSchedule(schedule: ReadScheduleWithClasses) {
+    this.scheduleService.openEditScheduleDialog(schedule);
+  }
 }
