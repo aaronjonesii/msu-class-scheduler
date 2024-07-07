8660a9f9ad8f298e54d95f5154d7f938eafb5272
Aaron Jones II <jonesaaron1996@gmail.com>
2024-07-06 23:36:39 -0400

    chore: update home page

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,11 +1,20 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatAnchor, MatButton } from "@angular/material/button";
+import { RouterLink } from "@angular/router";
+import { appRoutes } from "../../app.routes";
 
 @Component({
   selector: 'csb-home',
   standalone: true,
-  imports: [],
+  imports: [
+    MatButton,
+    MatAnchor,
+    RouterLink
+  ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class HomeComponent {}
+export class HomeComponent {
+  protected readonly appRoutes = appRoutes;
+}
