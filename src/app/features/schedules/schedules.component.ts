bdb912a48531b91317ef3447bdb07ef98655c06d
Aaron Jones II <jonesaaron1996@gmail.com>
2024-10-07 16:12:08 -0400

    chore(schedules): update filter description verbage

--- src/app/features/schedules/schedules.component.ts
+++ src/app/features/schedules/schedules.component.ts
@@ -6,13 +6,11 @@ import {
 import { SchedulesService } from "../../shared/services/schedules.service";
 import { toSignal } from "@angular/core/rxjs-interop";
 import { AuthService } from "../../shared/services/auth.service";
-import { first, of, switchMap } from "rxjs";
+import { of, switchMap } from "rxjs";
 import { MatButton } from "@angular/material/button";
 import { MatIcon } from "@angular/material/icon";
-import { Router } from "@angular/router";
 import { appRoutes } from "../../app.routes";
 import { LoggerService } from "../../shared/services/logger.service";
-import { ReadSchedule } from "../../shared/interfaces/schedule";
 import {
   SchedulesListComponent
 } from "../../shared/components/schedules-list/schedules-list.component";
@@ -21,10 +19,6 @@ import { KeyValuePipe } from "@angular/common";
 import { FormsModule } from "@angular/forms";
 import { Timestamp } from "@angular/fire/firestore";
 import { dateDifference } from "../../shared/utils/date-difference";
-import { MatDialog } from "@angular/material/dialog";
-import {
-  ScheduleFormDialogComponent, ScheduleFormDialogContract
-} from "../../shared/dialogs/schedule-form-dialog/schedule-form-dialog.component";
 
 export enum ScheduleFilterOption {
   MOST_RECENT = 'Most recent',
@@ -51,13 +45,11 @@ export class SchedulesComponent {
   private schedulesService = inject(SchedulesService);
   private authService = inject(AuthService);
   private logger = inject(LoggerService);
-  private dialog = inject(MatDialog);
-  private router = inject(Router);
 
   protected readonly appRoutes = appRoutes;
   protected readonly ScheduleFilterOption = ScheduleFilterOption;
 
-  schedulesFilter = signal(ScheduleFilterOption.MOST_RECENT);
+  schedulesFilter = signal(ScheduleFilterOption.ALL);
 
   user = toSignal(this.authService.authState$());
 
@@ -69,13 +61,15 @@ export class SchedulesComponent {
     ),
   );
 
+  mostRecentDays = 30;
+
   filterDescription = computed(() => {
     switch (this.schedulesFilter()) {
       case ScheduleFilterOption.MOST_RECENT:
-        return 'Recently updated schedules (within 30 days).';
+        return `Your ecently updated schedules (within ${this.mostRecentDays} days).`;
       case ScheduleFilterOption.ALL:
       default:
-        return 'All schedules.';
+        return 'All your schedules.';
     }
   })
 
@@ -89,7 +83,7 @@ export class SchedulesComponent {
 
           const differenceInDays = dateDifference(now, mostRecentDate.toDate());
 
-          return differenceInDays <= 30;
+          return differenceInDays <= this.mostRecentDays;
         });
       case ScheduleFilterOption.ALL:
       default: return this.schedules();
@@ -105,27 +99,7 @@ export class SchedulesComponent {
       return;
     }
 
-    const dialogRef = this.dialog.open(
-      ScheduleFormDialogComponent,
-      {
-        id: 'create-schedule-form-dialog',
-        width: '100%',
-        maxWidth: '600px',
-        data: { userId: user.uid } as ScheduleFormDialogContract,
-      },
-    );
-
-    dialogRef.afterClosed().pipe(first())
-      .forEach(async (schedule?: ReadSchedule) => {
-        if (!schedule) return;
-
-        await this.schedulesService.create(schedule)
-          .then((newDoc) => {
-            if (!newDoc) return;
-
-            this.router.navigate([appRoutes.scheduleDetail(newDoc.id)]);
-          });
-      });
+    this.schedulesService.openCreateDialog(user);
   }
 
   keepSameOrder = () => 1;
