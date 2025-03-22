405b8cd602be723fe53bda7a7712f03871ee587c
Aaron Jones II <jonesaaron1996@gmail.com>
2025-03-22 19:53:21 -0400

    feat(dialogs): semester plan course section form dialog

---
+++ src/app/shared/dialogs/semester-plan-course-section-form-dialog/semester-plan-course-section-form-dialog.component.ts
@@ -0,0 +1,52 @@
+import { ChangeDetectionStrategy, Component, inject, OnInit, signal } from '@angular/core';
+import { ReadCourseSection } from "../../interfaces/course-section";
+import {
+  MAT_DIALOG_DATA,
+  MatDialogActions, MatDialogClose,
+  MatDialogContent,
+  MatDialogTitle
+} from "@angular/material/dialog";
+import { CourseSectionForm } from "../../forms/course-section-form";
+import { MatButton } from "@angular/material/button";
+import {
+  CourseSectionFormComponent
+} from "../../components/course-section-form/course-section-form.component";
+import { JsonPipe } from "@angular/common";
+
+export interface SemesterPlanCourseSectionFormDialogContract {
+  section?: ReadCourseSection
+}
+
+@Component({
+  selector: 'csb-semester-plan-course-section-form-dialog',
+  standalone: true,
+  imports: [
+    MatDialogTitle,
+    MatDialogContent,
+    MatDialogActions,
+    MatButton,
+    MatDialogClose,
+    CourseSectionFormComponent,
+    JsonPipe
+  ],
+  templateUrl: './semester-plan-course-section-form-dialog.component.html',
+  styleUrl: './semester-plan-course-section-form-dialog.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SemesterPlanCourseSectionFormDialogComponent implements OnInit {
+  private contract = inject<SemesterPlanCourseSectionFormDialogContract>(MAT_DIALOG_DATA);
+
+  sectionForm = signal(new CourseSectionForm())
+
+  editing = signal(false);
+
+  ngOnInit(): void {
+    if (!this.contract) return;
+
+    this.editing.set(true);
+
+    this.sectionForm.set(
+      new CourseSectionForm(this.contract.section),
+    );
+  }
+}
